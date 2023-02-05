ffc5df708f277439cc6218713e9a4d00f6bc4098
Mayank Kumar Patel <72437985+night05fury@users.noreply.github.com>
2023-02-05 23:58:58 +0530

    Completed basic structure of the project.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import "./App.css"
+import SearchIcon from './search.svg';
+
+
+//58a66706    API Key for OMBD 
+
+const API_URL='http://www.omdbapi.com?apikey=58a66706';
+const movie1={
+    "Title":"Avengers",
+    "Year":"2015",
+    "imdbID":"tt2586634",
+    "Type":"movie",
+    "Poster":"N/A"
+}
+
+
+
 
 const App=()=>{
+
+    const searchMovies = async(title)=>{
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data=await response.json();
+
+        console.log(data.Search);
+        
+
+    }
+    useEffect(()=>{
+        searchMovies('Avengers');
+    },[]);
+
+
+
     return(
-        <h1>
-            App
-        </h1>
+    <div className="app">
+            <h1>
+                Box Office
+            </h1>
+            <div className="search">
+                <input
+                    placeholder="Search for movies"
+                    value="Avengers"
+                    onChange={()=>{}}
+                />
+                <img src={SearchIcon}
+                alt="Search"
+                onClick={()=>{}}
+                />
+            </div>
+
+            <div className='container'>
+                <div className="movie">
+                    <p>
+                        {movie1.Year}
+                    </p>
+                <div className=''>
+                    <img src={movie1.Poster!=='N/A'? movie1.Poster :'https://via.placeholder.com/400'} alt={movie1.Title}/>
+                </div>
+                <div >
+                    <span>{movie1.Type}</span>
+                    <h3>{movie1.Title}</h3>
+                </div>
+                </div>
+            </div>
+    </div>
     );
 }
 
